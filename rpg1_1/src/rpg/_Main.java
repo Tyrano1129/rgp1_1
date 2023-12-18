74875c2fd1202671dba78c7e453bbdcb3e340135
pyeonm30 <149234305+pyeonm30@users.noreply.github.com>
2023-12-18 11:28:49 +0900

    create project
    create project

---
+++ rpg1_1/src/rpg/_Main.java
@@ -0,0 +1,23 @@
+package rpg;
+
+public class _Main {
+
+	public static void main(String[] args) {
+		// JDK 11
+		// UTF-8
+		
+		// 절대 마스터(master , main) 브랜치에서 작업하지 않는다 
+
+		// 1. 새 브랜치 생성
+		// ㄴ 이름 규칙 : #번호-영문풀네
+		
+		// 2. 내 브랜치로 스위치 
+		// ㄴ 프로젝트 우측에 [IT #1-yeonmipark] 
+		//    바뀐 브랜치 확인		
+		// ㄴ rpg1_1 작업 시작
+		// ㄴ 커밋 & 푸시 
+		
+		new Game();
+	}
+
+}
